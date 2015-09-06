608ed9eea41ef83520222ed6c8e2fe3de5c4ab7b
steven harradine <steven.harradine@telus.com>
2015-09-06 08:51:03 -0400

    website

---
+++ assets/Web Site/download/dynamic.js
@@ -0,0 +1,23 @@
+function nav () {
+	document.write ("<br><br>");
+	document.write ("<a href=\"../index.html\">Home</a><br>");
+	document.write ("<img src=\"../images/left.png\"><img src=\"../images/center.png\" class=\"navDiv\"><img src=\"../images/right.png\"><br>");
+	document.write ("<a href=\"../about.html\">About Us</a><br>");
+	document.write ("<img src=\"../images/left.png\"><img src=\"../images/center.png\" class=\"navDiv\"><img src=\"../images/right.png\"><br>");
+	document.write ("<a href=\"../download.html\">Download</a><br>");
+	document.write ("<img src=\"../images/left.png\"><img src=\"../images/center.png\" class=\"navDiv\"><img src=\"../images/right.png\"><br>");
+	document.write ("<a href=\"../help.html\">Help</a><br>");
+}
+
+function bottomNav () {
+	document.write ("<span class=\"subLinksLinks\"><img src=\"../images/redDot.png\"> <a href=\"../index.html\">Home</a></span>");
+	document.write ("<span class=\"subLinksLinks\"><img src=\"../images/redDot.png\"> <a href=\"../about.html\">About Us</a></span>");
+	document.write ("<span class=\"subLinksLinks\"><img src=\"../images/redDot.png\"> <a href=\"../contact.html\">Contact</a></span>");
+	document.write ("<br>");
+	document.write ("&copy 2004 Virtual Breadboard");
+}
+
+function news() {
+	document.write("<h1>News:</h1>");
+	document.write("The breadboard is in its final stage of production before testing can begin.  The date is expected to be around January 10<sup>th</sup>, 2005.  If you or your school would like to participate in testing please email me at <i>Insert email address</i>");
+}
